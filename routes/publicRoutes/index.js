7be1f4ac39865c8127224d81bb0ba33455ea71c2
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-08-04 11:44:50 -0700

    Finished dummy set up for handlebars rendering

--- routes/publicRoutes/index.js
+++ routes/publicRoutes/index.js
@@ -10,9 +10,11 @@ const publicController = require('./../../controllers/publicController');
 
 */
 
+router.route('*').get(publicController.getHomePage);
+router.route('/signin').get(publicController.getSignInPage);
+router.route('/signup').get(publicController.getSignUpPage)
 router.route('/:username').get(publicController.getUserPage);
+router.route('/settings/:username').get(publicController.getUserSettings)
 router.route('/posts/:postID').get(publicController.getPostPage);
-router.route('/home').get(publicController.getHomePage);
-router.route('*').get(publicController.getSignInPage);
 
 module.exports = router;
