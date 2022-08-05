d923eae43f1087e6ebdcd7e9f7f39787afa86950
Michael Harris <snufalufakis@gmail.com>
2022-08-04 20:26:20 -0700

    changes to server.js for cloudinary

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const exphbs = require('express-handlebars');
 const expsesh = require('express-session');
+const cloudinary = require('cloudinary');
 
 const SequelizeStore = require('connect-session-sequelize')(expsesh.Store);
-const sequelize = require('./config/connection');
-const routes = require('./routes');
+const sequelize = require('../../Offload/config/connection');
+const routes = require('../../Offload/routes');
 
 // handlebars helpers
-const helpers = require('./utils/helpers');
+const helpers = require('../../Offload/utils/helpers');
 
 // handlebars init
 const hbs = exphbs.create({
@@ -27,6 +28,14 @@ const sessionSettings = {
 };
 
 const app = express();
+// Added cloudinary config
+const cloudinary = require('cloudinary').v2
+// cloudinary config
+cloudinary.config({
+    cloud_name: process.env.CLOUD_NAME,
+    api_key: process.env.API_KEY,
+    api_secret: process.env.API_SECRET
+  });
 
 const PORT = process.env.PORT || 3001;
 
