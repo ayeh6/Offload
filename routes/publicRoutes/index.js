ac0357df45c1d6094e384f746ad22a51e500e408
Jmonty94 <103618671+jmonty94@users.noreply.github.com>
2022-08-06 10:22:50 -0700

    Created new route for the create post page

--- routes/publicRoutes/index.js
+++ routes/publicRoutes/index.js
@@ -15,6 +15,7 @@ router.route('/signin').get(publicController.getSignInPage);
 router.route('/signup').get(publicController.getSignUpPage)
 router.route('/:username').get(publicController.getUserPage);
 router.route('/settings/:username').get(publicController.getUserSettings)
+router.route('/createPost').get(publicController.getCreatePostPage)
 router.route('/posts/:postID').get(publicController.getPostPage);
 router.route('*').get(publicController.getHomePage);
 
